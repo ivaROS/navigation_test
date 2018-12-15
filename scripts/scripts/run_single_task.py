87f8c3d31b8dc8330f4a1cf41118969d319cf081
Justin Smith <jssmith@gatech.edu>
2018-12-15 00:21:08 -0500

    Added bumper detector to pioneer launch files

--- scripts/scripts/run_single_task.py
+++ scripts/scripts/run_single_task.py
@@ -16,7 +16,7 @@ goal = [15,15,0]
 # book chapter testing
 task = {'seed':seed, 'scenario':'fourth_floor', 'num_barrels':num_barrels, 'target_id':4, 'min_obstacle_spacing':min_obstacle_spacing}
 
-# task = {'seed':seed, 'scenario':'campus', 'num_barrels':num_barrels, 'target_id':1}
+task = {'seed':seed, 'scenario':'campus', 'num_barrels':num_barrels, 'target_id':1}
 
 # task = {'seed':seed, 'scenario':'sector'}
 
@@ -36,3 +36,5 @@ scenario.setupScenario()
 
 
 result = test_driver.run_test(goal_pose=scenario.getGoal())
+
+print(result)
