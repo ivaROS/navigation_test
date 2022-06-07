d186b66c27d3c040f0febec4e57da65ddc17c4f9
Justin Smith <jssmith@gatech.edu>
2022-06-07 17:25:33 -0400

    Test ways to ensure everything closes properly

--- scripts/tests/graceful_shutdown.py
+++ scripts/tests/graceful_shutdown.py
@@ -62,58 +62,6 @@ def run_queue_test(num):
     import types
 
 
-    class InterruptibleQueueWrapper(object):
-
-        def __init__(self, queue, shutdown_event, sleep_time=1, name="queue"):
-            self.queue = queue
-            self.shutdown_event = shutdown_event
-            self.sleep_time = sleep_time
-            self.name = name
-
-        def put(self, data):
-            while not self.shutdown_event.is_set():
-                try:
-                    self.task_queue.put(data, block=False)
-                except queue.Full as e:
-                    if not is_full:
-                        print("[" + str(self.name) + "] is full!, will try to add task later")
-                    is_full = True
-                    time.sleep(self.sleep_time)
-                else:
-                    break
-
-        def get(self):
-
-            class NextGetter(object):
-
-                class DoneException(BaseException): pass
-
-                def __enter__(myself):
-                    while not self.tc.shutdown_event.is_set():
-                        try:
-                            task = self.tc.task_queue.get(block=False)
-                        except queue.Empty as e:
-                            if not is_empty:
-                                print("No task in [" + str(self.name) + "]!")
-                            is_empty = True
-                            time.sleep(1)
-                        else:
-                            return task
-
-                    raise NextGetter.DoneException()
-
-                def __exit__(self, exc_type, exc_val, exc_tb):
-                    if issubclass(exc_type, NextGetter.DoneException):
-                        return True
-
-
-
-
-
-
-
-
-
     class TaskCommunication(object):
 
         #Takes in lists (or generators) of tasks and transfers them, in order, to the task queue
@@ -250,6 +198,196 @@ def run_queue_test(num):
 
 
 
+
+def run_queue_wrapper_test(num):
+
+    import threading
+    import queue
+    import types
+
+
+    class GracefulShutdownException(BaseException): pass
+
+    class InterruptibleQueueWrapper(object):
+
+        def __init__(self, queue, shutdown_event, sleep_time=1, name="queue"):
+            self.queue = queue
+            self.shutdown_event = shutdown_event
+            self.sleep_time = sleep_time
+            self.name = name
+
+        def put(self, task):
+            is_full = False
+            while not self.shutdown_event.is_set():
+                try:
+                    self.queue.put(task, block=False) #, block=True, timeout=self.sleep_time)
+                except queue.Full as e:
+                    if not is_full:
+                        print("[" + str(self.name) + "] is full!, unable to add task!")
+                    is_full = True
+                    time.sleep(self.sleep_time)
+                else:
+                    print("Added task " + str(task) + " to [" + str(self.name) + "]")
+                    break
+
+        def get(self):
+
+            class NextGetter(object):
+
+                #class DoneException(BaseException): pass
+                #
+                # def __init__(self, queue, name):
+                #     self.queue = queue
+                #     self.name = name
+
+                def __enter__(myself):
+                    is_empty = False
+
+                    while not self.shutdown_event.is_set():
+                        try:
+                            task = self.queue.get(block=False)
+                        except queue.Empty as e:
+                            if not is_empty:
+                                print("No task in [" + str(self.name) + "]!")
+                            is_empty = True
+                            time.sleep(1)
+                        else:
+                            print("Got task " + str(task) + " from [" + str(self.name) + "]!")
+                            return task
+
+                    raise GracefulShutdownException()
+
+                def __exit__(myself, exc_type, exc_val, exc_tb):
+                    #if exc_type is not None and issubclass(exc_type, NextGetter.DoneException):
+                    #    return True
+                    self.queue.task_done()
+
+            return NextGetter()
+
+    class TaskCommunication(object):
+
+        #Takes in lists (or generators) of tasks and transfers them, in order, to the task queue
+        class TaskQueuer(object):
+
+            def __init__(self, task_queue, shutdown_event):
+                self.task_queue = InterruptibleQueueWrapper(queue=task_queue, shutdown_event=shutdown_event, sleep_time=1, name="Task Queue")
+                self.thread = threading.Thread(target=self.run)
+                self.task_input_queue = InterruptibleQueueWrapper(queue=queue.Queue(10), shutdown_event=shutdown_event, sleep_time=1, name="Task Input Queue")
+                self.shutdown_event = shutdown_event
+                self.thread.start()
+
+            def run(self):
+
+                try:
+                    while not self.shutdown_event.is_set():
+                        with self.task_input_queue.get() as task_obj:
+                            task_it = task_obj() if isinstance(task_obj, types.GeneratorType) else task_obj if isinstance(task_obj, list) else None
+
+                            for t in task_it:
+                                self.task_queue.put(t)
+                except GracefulShutdownException as e:
+                    print(str(e))
+
+                print("Shutting down task queuer!")
+
+
+            def add_tasks(self, tasks):
+                self.task_input_queue.put(task=tasks)
+
+            def wait_to_finish(self):
+                print("Wait for task input queue to join...")
+                self.task_input_queue.queue.join()
+                print("Task input queue joined!")
+
+
+        def __init__(self):
+            self.task_queue = mp.JoinableQueue(maxsize=10)
+            self.result_queue = mp.JoinableQueue(maxsize=10)
+            self.shutdown_event = mp.Event()
+            self.task_adder = TaskCommunication.TaskQueuer(task_queue=self.task_queue, shutdown_event=self.shutdown_event)
+
+        def shutdown(self):
+            self.shutdown_event.set()
+
+        def wait_to_finish(self):
+            self.task_adder.wait_to_finish()
+
+            print("Wait for task queue to join...")
+            self.task_queue.join()
+            print("Task queue joined!")
+            self.shutdown()
+
+        def add_tasks(self, tasks):
+            self.task_adder.add_tasks(tasks=tasks)
+
+
+    class Worker(mp.Process):
+
+        def __init__(self, tc, num=0):
+            super(Worker, self).__init__()
+            self.tc = tc
+            self.num = num
+
+        def run(self):
+            is_empty = False
+            while not self.tc.shutdown_event.is_set():
+                try:
+                    task = self.tc.task_queue.get(block=False)
+                except queue.Empty as e:
+                    if not is_empty:
+                        print("No task in task queue!")
+                    is_empty = True
+                    time.sleep(1)
+                else:
+                    is_empty = False
+                    msg = "[" + str(self.num) + "]: Do some work (" + str(task)
+                    print(msg)
+                    do_stuff()
+                    #self.tc.result_queue.put
+                    self.tc.task_queue.task_done()
+
+            print("[" + str(self.num) + "]: Exiting!")
+
+    def make_tasks_gen(num):
+        for i in range(num):
+            yield {"index": i}
+
+    def make_tasks_list(num):
+        return [i for i in make_tasks_gen(num=num)]
+
+
+    tc = TaskCommunication()
+
+    processes = [Worker(tc=tc, num=i) for i in range(num)]
+    print("Created [" + str(num) + "] Workers")
+
+    tc.add_tasks(make_tasks_list(num=30))
+
+    for p in processes:
+        p.start()
+    print("Started Workers")
+
+    for _ in range(3):
+        tc.add_tasks(make_tasks_list(num=10))
+    tc.add_tasks(make_tasks_list(num=50))
+
+
+    if False:
+        time.sleep(20)
+    else:
+        tc.wait_to_finish()
+
+    print("Sending shutdown signal...")
+    tc.shutdown()
+
+    for p in processes:
+        p.join()
+
+    print("All Done")
+
+
+
+
 def InterruptibleQueue(maxsize=0):
     import multiprocessing as mp
     import multiprocessing.queues
@@ -266,4 +404,5 @@ def InterruptibleQueue(maxsize=0):
 
 if __name__ == "__main__":
     #run_basic_test(2)
-    run_queue_test(1)
+    #run_queue_test(1)
+    run_queue_wrapper_test(5)
