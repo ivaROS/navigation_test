cda257afb96b8e963edd212177c2b473c63ff3f8
Justin Smith <jssmith@gatech.edu>
2022-06-09 00:00:28 -0400

    Add proof-of-concept processing pipeline with clean shutdown

--- scripts/tests/graceful_shutdown.py
+++ scripts/tests/graceful_shutdown.py
@@ -407,7 +407,7 @@ def run_queue_wrapper_test(num):
 
 
 
-def interruptible_queue_test(num):
+def processing_stages_test(num):
 
     import threading
     import queue
@@ -439,6 +439,9 @@ def interruptible_queue_test(num):
             self.shutdown_event = shutdown_event
             self.sleep_time = sleep_time
             self.name = name
+
+        def event(self):
+            return self.shutdown_event
 
         def put(self, task):
             is_full = False
@@ -484,98 +487,207 @@ def interruptible_queue_test(num):
         def join(self):
             return self.queue.join()
 
-    class TaskCommunication(object):
-
-        #Takes in lists (or generators) of tasks and transfers them, in order, to the task queue
-        class TaskQueuer(object):
-
-            def __init__(self, task_queue, shutdown_event):
-                self.task_queue = task_queue #InterruptibleQueueWrapper(queue=task_queue, shutdown_event=shutdown_event, sleep_time=1, name="Task Queue")
-                self.thread = threading.Thread(target=self.run)
-                self.task_input_queue = InterruptibleQueueWrapper(queue=queue.Queue(10), shutdown_event=shutdown_event, sleep_time=1, name="Task Input Queue")
-                self.shutdown_event = shutdown_event
-                self.thread.start()
-
-            def run(self):
-
-                try:
-                    while True:
-                        with self.task_input_queue.get() as task_obj:
-                            task_it = task_obj if isinstance(task_obj, types.GeneratorType) else task_obj if isinstance(task_obj, list) else None
-                            #Only pass tasks on if not trying to shutdown
-                            if not self.shutdown_event.is_set():
-                                for t in task_it:
-                                    self.task_queue.put(t)
-                            else:
-                                print("Task input queue: Ignoring tasks " + str(task_it))
-
-                except GracefulShutdownException as e:
-                    print(str(e))
-
-                print("Shutting down task queuer!")
 
 
-            def add_tasks(self, tasks):
-                self.task_input_queue.put(task=tasks)
+    class TaskProcessingStage(object):
 
-            def wait_to_finish(self):
-                print("Wait for task input queue to join...")
-                self.task_input_queue.queue.join()
-                print("Task input queue joined!")
-
-
-        def __init__(self):
-            self.shutdown_event = mp.Event()
-
-            self.task_queue = InterruptibleQueueWrapper(queue=mp.JoinableQueue(maxsize=10), shutdown_event=self.shutdown_event, sleep_time=1,
-                                                        name="Task Queue")
-
-            self.result_queue = mp.JoinableQueue(maxsize=10)
-            self.task_adder = TaskCommunication.TaskQueuer(task_queue=self.task_queue, shutdown_event=self.shutdown_event)
-
-        def shutdown(self):
-            self.shutdown_event.set()
-
-        def wait_to_finish(self):
-            self.task_adder.wait_to_finish()
-
-            print("Wait for task queue to join...")
-            self.task_queue.join()
-            print("Task queue joined!")
-            self.shutdown()
-
-        def add_tasks(self, tasks):
-            self.task_adder.add_tasks(tasks=tasks)
+        def __init__(self, name, use_mp=False):
+            self.name=name
+            self.use_mp=use_mp
+            self.shutdown_now = mp.Event()
+            self.shutdown_when_ready = mp.Event()
+            self.input_queue = None
+            self.output_queue = None
+            self.prev_stage =  None
+            self.next_stage = None
+            self.task_exec = mp.Process(target=self.run, daemon=False) if self.use_mp else threading.Thread(target=self.run, daemon=False)
 
 
-    class Worker(mp.Process):
+        def set_input_stage(self, stage):
+            if stage is not None:
+                stage.set_next_stage(self)
+                #stage.output_queue = self.input_queue
 
-        def __init__(self, tc, num=0):
-            super(Worker, self).__init__()
-            self.tc = tc
-            self.num = num
+        def set_next_stage(self, stage):
+            self.next_stage = stage
+            self.output_queue = stage.input_queue
 
-        def signal_handler(self, signum, frame):
-            print("[" + str(self.num) + "]: Received signal " + str(signum)) # + "\n\n" + str(frame))
+        def start(self):
+            print(self.name + ": Starting " + ("Process" if self.use_mp else "Thread"))
+            self.task_exec.start()
 
         def run(self):
-            signal.signal(signal.SIGINT, self.signal_handler)
-            signal.signal(signal.SIGTERM, self.signal_handler)
-
+            if self.use_mp:
+                signal.signal(signal.SIGINT, self.signal_handler)
+                signal.signal(signal.SIGTERM, self.signal_handler)
             try:
-                while True: #not self.tc.shutdown_event.is_set():
-                    with self.tc.task_queue.get() as task:
-                        if not self.tc.shutdown_event.is_set():
-                            msg = "[" + str(self.num) + "]: Do some work (" + str(task)
-                            print(msg)
-                            do_stuff()
+                while True:
+                    with self.input_queue.get() as task:
+                        if not self.shutdown_now.is_set():
+                            self.process_task(task=task)
                         else:
-                            print("[" + str(self.num) + "]: Ignoring task " + str(task))
+                            self.ignore_task(task=task)
 
             except GracefulShutdownException as e:
                 print(str(e))
 
-            print("[" + str(self.num) + "]: Exiting!")
+            self.done()
+
+        def signal_handler(self, signum, frame):
+            print("[" + str(self.name) + "]: Received signal " + str(signum))
+
+        def process_task(self, task):
+            pass
+
+        def ignore_task(self, task):
+            print(self.name + ": Ignoring task " + str(task))
+
+        def shutdown(self):
+            self.shutdown_now.set()
+            self.wait_for_finish()
+
+        def join_input(self):
+            self.input_queue.shutdown_event.set()
+            print(self.name + ": Waiting to join queue [" + str(self.input_queue.name) + "]")
+            self.input_queue.join()
+            print(self.name + ": Emptied input queue [" + str(self.input_queue.name) + "]")
+
+        def join_exec(self):
+            self.task_exec.join()
+            print(self.name + ": Joined exec " + ('Thread' if not self.use_mp else 'Process'))
+
+        def wait_for_finish(self):
+            print(self.name + ": Waiting for finish")
+            self.join_input()
+            self.join_exec()
+
+            if self.next_stage is not None:
+                if self.shutdown_now.is_set():
+                    self.next_stage.shutdown()
+                else:
+                    self.next_stage.wait_for_finish()
+
+        #Not needed?
+        def done(self):
+            print(self.name + ": Finished processing")
+
+
+
+    # Takes in lists (or generators) of tasks and transfers them, in order, to the task queue
+    class TaskInput(TaskProcessingStage):
+
+        def __init__(self):
+            super(TaskInput,self).__init__(name="Task Input", use_mp=False)
+            self.input_queue = InterruptibleQueueWrapper(queue=queue.Queue(10), shutdown_event=self.shutdown_when_ready, sleep_time=1,
+                                                              name="Task Input Queue")
+
+        def process_task(self, task):
+            task_it = task if isinstance(task, types.GeneratorType) else task if isinstance(task, list) else None
+            for t in task_it:
+                self.output_queue.put(t)
+
+        def add_tasks(self, tasks):
+            self.input_queue.put(task=tasks)
+
+    class ResultRecorder(TaskProcessingStage):
+
+        def __init__(self):
+            super(ResultRecorder, self).__init__(name="Result Recorder", use_mp=False)
+            self.input_queue = InterruptibleQueueWrapper(queue=mp.JoinableQueue(maxsize=10), shutdown_event=self.shutdown_when_ready, sleep_time=1,
+                                                        name="Result Queue")
+
+        def run(self):
+            #with filewriter
+            super(ResultRecorder, self).run()
+            print("Finished processing all current results")
+
+        def process_task(self, task):
+            print("Processed result " + str(task))
+
+
+    """
+    When each component should stop and how it should be told:
+        Task source: task_input_queue
+            wait_for_finish: sets task_input_queue's shutdown signal and calls join on it; (also join the input thread)
+            once joined, calls wait_for_finish on workers
+            
+        Worker pool: task_queue: 
+            wait_for_finish: sets task_queue's shutdown signal and calls join on it, then on workers themselves
+                        
+            
+                      
+    """
+
+    class Worker(TaskProcessingStage):
+
+        def __init__(self, task_queue, num):
+            super(Worker, self).__init__(name="Worker_" + str(num), use_mp=True)
+            self.input_queue = task_queue
+
+        def process_task(self, task):
+            msg = "[" + str(self.name) + "]: Do some work (" + str(task) + ")"
+            print(msg)
+            do_stuff()
+            task["result"] = 'Done'
+            self.output_queue.put(task)
+
+
+    class WorkerPool(TaskProcessingStage):
+
+        def __init__(self, num_workers=1):
+            super(WorkerPool,self).__init__(name="Worker Pool", use_mp=False)
+            self.input_queue = InterruptibleQueueWrapper(queue=mp.JoinableQueue(maxsize=10), shutdown_event=self.shutdown_when_ready, sleep_time=1,
+                                                        name="Task Queue")
+            self.workers = [Worker(task_queue=self.input_queue, num=i) for i in range(num_workers)]
+            for w in self.workers:
+                w.shutdown_now = self.shutdown_now
+                w.shutdown_when_ready = self.shutdown_when_ready
+
+        def start(self):
+            print(self.name + ": Starting workers")
+            for w in self.workers:
+                w.start()
+
+        def set_next_stage(self, stage):
+            super(WorkerPool,self).set_next_stage(stage=stage)
+            for w in self.workers:
+                w.set_next_stage(stage=stage)
+
+        def join_exec(self):
+            print(self.name + ": Joining workers")
+            for w in self.workers:
+                w.join_exec()
+            print(self.name + ": Joined all workers")
+
+
+
+    class TaskProcessingPipeline(object):
+
+        def __init__(self, num_workers):
+            self.task_input = TaskInput()
+            self.workers = WorkerPool(num_workers=num_workers)
+            self.result_recorder = ResultRecorder()
+
+            self.stages = [self.task_input, self.workers, self.result_recorder]
+            for ind in range(1,len(self.stages)):
+                self.stages[ind].set_input_stage(self.stages[ind-1])
+
+            #self.workers.set_input_stage(stage=self.task_input)
+            #self.result_recorder.set_input_stage(stage=self.workers)
+
+        def start(self):
+            for stage in self.stages:
+                stage.start()
+
+        def add_tasks(self, tasks):
+            self.task_input.add_tasks(tasks=tasks)
+
+        def wait_for_finish(self):
+            self.stages[0].wait_for_finish()
+
+        def shutdown(self):
+            self.stages[0].shutdown()
+
 
     def make_tasks_gen(num):
         for i in range(num):
@@ -585,43 +697,24 @@ def interruptible_queue_test(num):
         return [i for i in make_tasks_gen(num=num)]
 
 
-    tc = TaskCommunication()
-
-    processes = [Worker(tc=tc, num=i) for i in range(num)]
-    print("Created [" + str(num) + "] Workers")
-
-    def shutdown():
-        tc.shutdown()
+    tpp = TaskProcessingPipeline(num_workers=num)
+    tpp.start()
 
     def signal_handler(signum, frame):
         print("Main process received signal " + str(signum))
         #print("Main process: \n\n" + str(signum) + "\n\n" + str(frame))
-        shutdown()
+        tpp.shutdown()
 
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
     #tc.add_tasks(make_tasks_list(num=30))
     for _ in range(5):
-        tc.add_tasks(make_tasks_list(num=100))
+        tpp.add_tasks(make_tasks_list(num=100))
 
-    for p in processes:
-        p.start()
-    print("Started Workers")
+    tpp.add_tasks(make_tasks_gen(num=500))
 
-    tc.add_tasks(make_tasks_gen(num=500))
-
-
-    if False:
-        time.sleep(20)
-    else:
-        tc.wait_to_finish()
-
-    print("Sending shutdown signal...")
-    tc.shutdown()
-
-    for p in processes:
-        p.join()
+    tpp.wait_for_finish()
 
     print("All Done")
 
@@ -757,4 +850,5 @@ if __name__ == "__main__":
     #run_queue_test(1)
     #run_queue_wrapper_test(5)
     #interruptible_queue_object_test()
-    interruptible_queue_keyboard_interrupt()
+    #interruptible_queue_keyboard_interrupt()
+    processing_stages_test(num=8)
